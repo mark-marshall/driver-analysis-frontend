20f3951b27c88f50c0ef955f22dadefdbfcb77d8
mark-marshall <mark.marshallgp@gmail.com>
2019-11-02 14:57:31 +0000

    added chart.js and basic chart formating

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Chart from './Components/Chart';
 function App() {
   const initState = {
     data: '',
+    req: '',
   };
   const [data, setData] = useState(initState.data);
+  const [req, setReq] = useState(initState.req);
 
   const loadData = (type, body) => {
     analyse(type, body).then(res => {
       setData(res);
+      setReq({...body,type});
     });
   };
 
   return (
     <div>
       <Dropdown loadData={loadData} />
-      <Chart data={data} />
+      <Chart data={data} req={req}/>
     </div>
   );
 }
--- src/Components/Chart.jsx
+++ src/Components/Chart.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
 
-function Chart({ data }) {
-  return <div>chart</div>;
+function Chart({ data, req }) {
+  const chartData = {
+    labels: [],
+    datasets: [
+      {
+        title: '',
+        label: 'Delta',
+        backgroundColor: 'rgb(255, 99, 132)',
+        data: [],
+      },
+    ],
+  };
+  for (let label in data) {
+    chartData['labels'].push(label);
+    chartData['datasets'][0]['data'].push(data[label]);
+  }
+  const chartOptions = {
+    title: {
+      display: true,
+      text: '',
+    },
+    legend: {
+      display: false,
+    }
+  };
+  return data && req ? <Bar data={chartData} options={chartOptions} /> : null;
 }
 
 export default Chart;
